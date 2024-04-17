83bc7e675f951c2c554970350ff89526b3111539
Fernando Alberto Miranda Bonomi <fmirandabonomi@herrera.unt.edu.ar>
2024-04-17 18:47:43 -0300

    Configuración de pruebas para correcta ejecución en hardware

--- test/unity_config.c
+++ test/unity_config.c
@@ -1,8 +1,13 @@
 #include "serie.h"
 #include "unity_config.h"
+#include "timer_sistema.h"
 
 void Unity_outputStart(void){
+    TS_init();
     Serie_init(115200);
+#ifdef ESPERA_PRUEBAS
+    TS_espera(ESPERA_PRUEBAS);
+#endif
 }
 
 void Unity_outputChar(int c){
